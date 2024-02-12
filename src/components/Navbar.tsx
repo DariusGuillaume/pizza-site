666f2fea3815009a0dba639ce3a8879fd0989cb9
DariusGuillaume <dariusguillaume2@gmail.com>
2024-02-12 11:33:21 -0500

    Added right link login in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
   <Image src = "/phone.png" alt = " " width = {20} height = {20} />
   <span> 123-456-7890 </span>
 </div>
-      {!user ? (<Link href = "/menu"> Menu </Link>) :(
+      {!user ? (<Link href = "/login"> Login </Link>) :(
       <Link href = "/orders">Orders</Link> )}
       <CartIcon/>
       </div>
